73bb7f6163841a718114dbf85e90bcfeeea51dec
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-01-05 22:08:01 +0900

    chore : 헤더에 x표시 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
 import styled from "styled-components";
 
-const Logo = styled.span`
+const LogoWrapper = styled.div`
+  position: relative;
   display: flex;
   width: 100vw;
   justify-content: center;
+  align-items: center;
   padding-top: 45px;
+`;
+
+const Logo = styled.span`
   font-size: 24px;
   font-family: "White Angelica";
 `;
 
+const CloseButton = styled.span`
+  position: absolute;
+  right: 6%;
+  font-size: 30px;
+  cursor: pointer;
+`;
+
 interface HeaderProps {
   type: "logo" | "list";
+  withClose?: boolean;
 }
 
-const Header = ({ type }: HeaderProps) => {
-  return <Logo>{type === "logo" ? "SumHeart" : "List"}</Logo>;
+const Header = ({ type, withClose }: HeaderProps) => {
+  return (
+    <LogoWrapper>
+      <Logo>{type === "logo" ? "SumHeart" : "List"}</Logo>
+      {type === "logo" && withClose && <CloseButton>x</CloseButton>}
+    </LogoWrapper>
+  );
 };
 
 export default Header;
