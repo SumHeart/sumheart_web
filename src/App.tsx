0bb3b847d4cfa4b2118588a138bc30b9ff897b8b
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-01-07 00:15:56 +0900

    Feat : 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,20 @@ import WriteName from "./pages/WriteName";
 import Main from "./pages/Main";
 import ConntextFamily from "./pages/ConnectFamily";
 import QuestionDetail from "./pages/QuestionDetail";
-// import AnswerPage from "./pages/AnswerPage";
 import AnswerModal from "./pages/AnswerModal";
-// import FeelModal from "./pages/FeelModal";
 import AnswerPage from "./pages/AnswerPage";
 import ChatPage from "./pages/ChatPage";
 import List from "./pages/List";
 
 const App = () => {
-  const [userName, setUserName] = useState("");
+  const [, setName] = useState("");
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ConntextFamily />} />
-        <Route
-          path="/write-name"
-          element={<WriteName setName={setUserName} />}
-        />
-        <Route path="/main" element={<Main userName={userName} />} />
+        <Route path="/write-name" element={<WriteName setName={setName} />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/question-detail" element={<QuestionDetail />} />
         <Route path="/answer" element={<AnswerModal />} />
         <Route path="/result" element={<AnswerPage />} />
