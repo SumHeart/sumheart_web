09f5192658874ace91d53af1a3c245f75229bf04
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-02-14 18:33:31 +0900

    feat: 로그인 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import AnswerModal from "./pages/AnswerModal";
 import AnswerPage from "./pages/AnswerPage";
 import ChatPage from "./pages/ChatPage";
 import List from "./pages/List";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ConntextFamily />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/write-name" element={<WriteName type="name" />} />
         <Route path="/write-petname" element={<WriteName type="petName" />} />
         <Route path="/main" element={<Main />} />
